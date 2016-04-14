0ae07c0d88eebcb535f20f129415413d3b45514c
REW-Obie <egwim.obie@realestatewebmasters.com>
2016-04-14 12:16:18 -0700

    update propTypes & move click handler for larger target

--- Components/Accordion/AccordionSection.jsx
+++ Components/Accordion/AccordionSection.jsx
@@ -6,6 +6,7 @@ import styles from './Accordion.css';
 class AccordionSection extends React.Component {
     static propTypes = {
         title: PropTypes.node.isRequired,
+        children: PropTypes.node.isRequired,
         initialIsOpen: PropTypes.bool
     };
     static defaultProps = {
@@ -26,9 +27,9 @@ class AccordionSection extends React.Component {
         const { title, children, styles } = this.props;
         const activeContent = isOpen ? styles['contentActive'] : '';
         const activeTitle = isOpen ? styles['titleActive'] : '';
-        console.log('state:', this.state);
-        return <section styleName='section'>
-            <h3 styleName='title' className={activeTitle} onClick={this.handleToggleOpen}>
+        // console.log('state:', this.state);
+        return <section styleName='section' onClick={this.handleToggleOpen}>
+            <h3 styleName='title' className={activeTitle}>
                 {title}
             </h3>
             <div styleName='content' className={activeContent}
