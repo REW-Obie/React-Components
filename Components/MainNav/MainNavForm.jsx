b31af34318ab062da8e24a882a7774dd29ec7e16
REW-Obie <egwim.obie@realestatewebmasters.com>
2016-04-15 10:28:15 -0700

    update option values to use id's set in index.html

--- Components/MainNav/MainNavForm.jsx
+++ Components/MainNav/MainNavForm.jsx
@@ -7,42 +7,35 @@ const redirectToUrl = url => {
 };
 
 class MainNavForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleSelectNav = this.handleSelectNav.bind(this);
-  }
+        constructor(props) {
+            super(props);
+            this.handleSelectNav = this.handleSelectNav.bind(this);
+        }
         handleSelectNav(){
-          const value = this.refs.select.value;
-          if (value !== "") {
-            redirectToUrl(value);
-          }
+            const value = this.refs.select.value;
+            if (value !== "") {
+                redirectToUrl(value);
+            }
         }
         render() {
-            const {children} = this.props;
 
             return <form styleName="form" action method="post">
                 <select name="js-nav nav_select" className='js-nav'
                   onChange= {this.handleSelectNav} ref='select'>
-                  <option value>Jump to...</option>
-              <option value="#accordion">&nbsp;&nbsp;&nbsp;&nbsp;Accordion</option>
-              <option value="#card1">&nbsp;&nbsp;&nbsp;&nbsp;Card 1</option>
-              <option value="#card2">&nbsp;&nbsp;&nbsp;&nbsp;Card 2</option>
-              <option value="#card3">&nbsp;&nbsp;&nbsp;&nbsp;Card 3</option>
-              <option value="#cardlist">&nbsp;&nbsp;&nbsp;&nbsp;Card List</option>
-              <option value="#carousel">&nbsp;&nbsp;&nbsp;&nbsp;Carousel</option>
-                  <option value="#commentbox">&nbsp;&nbsp;&nbsp;&nbsp;Comment Box</option>
-                  <option value="#Form">&nbsp;&nbsp;&nbsp;&nbsp;Form</option>
-                  <option value="#Grid">&nbsp;&nbsp;&nbsp;&nbsp;Grid</option>
-                  <option value="#Hero">&nbsp;&nbsp;&nbsp;&nbsp;Hero</option>
-                  <option value="#Icon">&nbsp;&nbsp;&nbsp;&nbsp;Icon</option>
-                  <option value="#Navigation">&nbsp;&nbsp;&nbsp;&nbsp;Navigation</option>
-                  <option value="#Media">&nbsp;&nbsp;&nbsp;&nbsp;Media</option>
-                  <option value="#Subscribe">&nbsp;&nbsp;&nbsp;&nbsp;Subscribe Form</option>
-                  <option value="#Tab">&nbsp;&nbsp;&nbsp;&nbsp;Tab</option>
-                  <option value="#Table">&nbsp;&nbsp;&nbsp;&nbsp;Table</option>
+                      <option value>Jump to...</option>
+                      <option value="#accordion">&nbsp;&nbsp;&nbsp;&nbsp;Accordion</option>
+                      <option value="#card1">&nbsp;&nbsp;&nbsp;&nbsp;Card 1</option>
+                      <option value="#card2">&nbsp;&nbsp;&nbsp;&nbsp;Card 2</option>
+                      <option value="#card3">&nbsp;&nbsp;&nbsp;&nbsp;Card 3</option>
+                      <option value="#cardlist">&nbsp;&nbsp;&nbsp;&nbsp;Card List</option>
+                      <option value="#carousel">&nbsp;&nbsp;&nbsp;&nbsp;Carousel</option>
+                      <option value="#commentbox">&nbsp;&nbsp;&nbsp;&nbsp;Comment Box</option>
+                      <option value="#media">&nbsp;&nbsp;&nbsp;&nbsp;Media</option>
+                      <option value="#Subscribe">&nbsp;&nbsp;&nbsp;&nbsp;Subscribe Form</option>
+                      <option value="#tabset">&nbsp;&nbsp;&nbsp;&nbsp;Tab Set</option>
                 </select>
                 <input type="submit" value="Go" className="u-isRemoved" />
-              </form>;
+            </form>;
         }
 }
 
